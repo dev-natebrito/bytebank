422ae0d7cf92961eb8cbe38c427e1a0ef97f9ee6
Nate <nathan.brito@mottu.com.br>
2022-03-22 18:04:34 -0300

    getters and setters class

--- src/com/bytebank/Account.java
+++ src/com/bytebank/Account.java
@@ -1,10 +1,42 @@
 package com.bytebank;
 
 public class Account {
-    double balance;
-    String holder;
-    int bankCode;
-    int number;
+    private double balance;
+    private String holder;
+    private int bankCode;
+    private int number;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public int getBankCode() {
+        return bankCode;
+    }
+
+    public void setBankCode(int bankCode) {
+        this.bankCode = bankCode;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 
     public void deposit(double balance) {
         this.balance += balance;
